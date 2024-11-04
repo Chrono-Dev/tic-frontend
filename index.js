129e1996c284360905fcd697777700fd117f836e
Isaac Vinícius <igvinilink7@gmail.com>
2024-11-04 19:34:19 -0300

    Commit inicial do backend

---
+++ index.js
@@ -0,0 +1,31 @@
+
+  // Função para obter o IP do usuário
+function getUserIP() {
+  fetch("https://api.ipify.org?format=json")
+    .then(response => response.json())
+    .then(data => {
+      console.log("IP do usuário:", data.ip);
+      // Agora você pode enviar o IP para o backend se necessário
+      sendIPToBackend(data.ip);
+    })
+    .catch(error => console.error("Erro ao obter IP:", error));
+}
+
+// Função para enviar o IP para o backend
+function sendIPToBackend(ip) {
+  fetch("http://localhost:3001/qr_code_accesses", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({user_ip: ip, location: 'RU'}),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.message || data.error);
+    })
+    .catch(error => console.error("Erro ao enviar IP:", error));
+}
+
+// Chama a função para obter o IP ao carregar a página
+window.onload = getUserIP;
